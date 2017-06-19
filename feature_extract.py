05ac7002773936e281ba088f9be48abd58e94440
Christopher Wells <cwellsny@nycap.rr.com>
2017-06-19 00:11:01 -0400

    Cleanup feature extract script
    Remove unused and commented out code from the script as it is no longer
    needed.

--- feature_extract.py
+++ feature_extract.py
@@ -7,49 +7,17 @@ import pandas as pd
 import sys
 
 def main():
+    """
+    $ python3 feature_extract.py CatalinaVars.csv test.csv
+    """
     data_file = sys.argv[1]
     output_file = sys.argv[2]
 
     data = pd.read_csv(data_file)
 
-    #n_partitions = 1
-    #new_data = extract_all(data, n_partitions)
     new_data = extract_partition(data)
 
     new_data.to_csv(output_file, index=False)
-    #print(new_data)
-
-"""
-def extract_all(data, n_paritions):
-    partitions = []
-    part_size = int(len(data) / n_paritions)
-    left_over = len(data) % n_paritions
-    for i in range(n_paritions):
-        start = part_size * i
-        if i == n_paritions - 1:
-            end = part_size * (i + 1) + left_over
-            print("%d - %d" % (start, end))
-            partitions.append(data.iloc[start:end])
-        else:
-            end = part_size * (i + 1)
-            print("%d - %d" % (start, end))
-            partitions.append(data.iloc[start:end])
-
-    new_parts = par_map(extract_partition, partitions)
-    return pd.concat(new_parts)
-
-def extract_partition(partition):
-    return partition.apply(extract_with_curve, axis=1)
-
-def par_map(func, partitions):
-    caclulations = []
-    for part in partitions:
-        calc = delayed(func)(part)
-        caclulations.append(calc)
-
-    results = delayed(caclulations).compute(get = multiprocessing.get)
-    return results
-"""
 
 def extract_with_curve(data):
     curve_file = get_curve_file_name(data["Numerical_ID"])
@@ -104,8 +72,6 @@ def magnitude_ratio(magnitudes):
     return points_above_median / float(magnitudes.size)
 
 def maximum_slope(times, magnitudes):
-    #slopes = [(magnitudes[i + 1] - magnitudes[i]) / (times[i + 1] - times[i]) \
-    #        for i in range(len(times) - 1)]
     max_slope = None
 
     for i in range(len(times) - 1):
