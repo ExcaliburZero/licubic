f5ef8210d013abad59b09fff5c563b97f060411c
Christopher Wells <cwellsny@nycap.rr.com>
2017-07-10 22:53:37 -0400

    Add more regression tests

--- test/test_feature_extract.py
+++ test/test_feature_extract.py
@@ -41,8 +41,10 @@ def try_expected(self, curves, expected_results, test):
         expected = expected_results[i]
         actual = test(curve)
 
-        if np.isnan(expected):
+        if isinstance(expected, float) and np.isnan(expected):
             self.assertTrue(np.isnan(actual))
+        elif isinstance(expected, np.ndarray):
+            np.testing.assert_allclose(actual, expected, rtol=1e-5)
         else:
             self.assertEqual(expected, actual)
 
@@ -283,3 +285,197 @@ class TestFeatureExtract(unittest.TestCase):
             return feature_extract.interquartile_range(magnitudes)
 
         try_expected(self, [TEST_CURVES[0]] + [TEST_CURVES[3]], expected_results, test)
+
+    """def test_robust_median_statistic(self):
+        expected_results = [
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+            errors = curve[ERRORS]
+            return feature_extract.robust_median_statistic(magnitudes, errors)
+
+        try_expected(self, TEST_CURVES[0], expected_results, test)
+        """
+
+    def test_peak_to_peak_variability(self):
+        expected_results = [
+            0.025227750525578158,
+            0.01570262392595306,
+            0.059019673224408174,
+            0.093227733404926516
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+            errors = curve[ERRORS]
+
+            return feature_extract.peak_to_peak_variability(magnitudes, errors)
+
+        try_expected(self, TEST_CURVES, expected_results, test)
+
+    def test_fourier_decomposition(self):
+        expected_results = [
+            np.array([4.44354063, 3.48308428, -2.47321523, 4.71882566, -0.18528359, 3.46454684, 1.74425983]),
+            np.array([2.548879, 0.750577, 5.5012, 1.516458, 1.09882, 1.035017, 3.423319])
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+            times = curve[TIMES]
+            order = 3
+
+            return feature_extract.fourier_decomposition(magnitudes, times, order)
+
+        try_expected(self, TEST_CURVES[1:2] + TEST_CURVES[3:], expected_results, test)
+
+    def test_fourier_decomposition_few_examples(self):
+        curve = TEST_CURVES[0]
+        magnitudes = curve[MAGNITUDES]
+        times = curve[TIMES]
+        order = 3
+
+        try:
+            feature_extract.fourier_decomposition(magnitudes, times, order)
+            self.assertTrue(False)
+        except Exception as e:
+            expected = "Too few examples for the specified order. Number of examples must be at least order * 2 + 1. Required: 7, Actual: 5"
+            actual = str(e)
+            self.assertEqual(expected, actual)
+
+    def test_fourier_R_1(self):
+        expected_results = [
+            np.array([1.354784, 0.994678]),
+            np.array([2.02039, 1.378962])
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+            times = curve[TIMES]
+            order = 3
+
+            coef = feature_extract.fourier_decomposition(magnitudes, times, order)
+
+            r_21 = feature_extract.fourier_R_1(coef, 2)
+            r_31 = feature_extract.fourier_R_1(coef, 3)
+
+            return np.array([r_21, r_31])
+
+        try_expected(self, TEST_CURVES[1:2] + TEST_CURVES[3:], expected_results, test)
+
+    def test_fourier_phi_1(self):
+        expected_results = [
+            np.array([4.761147, 2.88072]),
+            np.array([2.662789, 5.769273])
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+            times = curve[TIMES]
+            order = 3
+
+            coef = feature_extract.fourier_decomposition(magnitudes, times, order)
+
+            phi_21 = feature_extract.fourier_phi_1(coef, 2)
+            phi_31 = feature_extract.fourier_phi_1(coef, 3)
+
+            return np.array([phi_21, phi_31])
+
+        try_expected(self, TEST_CURVES[1:2] + TEST_CURVES[3:], expected_results, test)
+
+    def test_residual_bright_faint_ratio(self):
+        expected_results = [
+            1.9068891280947429,
+            0.42857142857143882,
+            1.0,
+            0.10073344824793815
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+
+            return feature_extract.residual_bright_faint_ratio(magnitudes)
+
+        try_expected(self, TEST_CURVES, expected_results, test)
+
+    def test_cumulative_sum_range(self):
+        expected_results = [
+            57.450000000000003,
+            105.80000000000001,
+            15.9,
+            172.5
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+
+            return feature_extract.cumulative_sum_range(magnitudes)
+
+        try_expected(self, TEST_CURVES, expected_results, test)
+
+    def test_von_neumann_eta(self):
+        expected_results = [
+            1.7328199052132705,
+            2.4761904761904781,
+            4.0,
+            0.74523380619980528
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+
+            return feature_extract.von_neumann_eta(magnitudes)
+
+        try_expected(self, TEST_CURVES, expected_results, test)
+
+    def test_mean_crosses(self):
+        expected_results = [
+            2,
+            2,
+            1,
+            2
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+
+            return feature_extract.mean_crosses(magnitudes)
+
+        try_expected(self, TEST_CURVES, expected_results, test)
+
+    def test_above_1std(self):
+        expected_results = [
+            0.2,
+            0.25,
+            0.0,
+            0.23076923076923078
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+
+            return feature_extract.above_1std(magnitudes)
+
+        try_expected(self, TEST_CURVES, expected_results, test)
+
+    def test_above_below_1std_slopes(self):
+        expected_results = [
+            (0.25, 0.25),
+            (0.14285714285714285, 0.14285714285714285),
+            (0.0, 0.0),
+            (0.16666666666666666, 0.16666666666666666)
+        ]
+
+        def test(curve):
+            magnitudes = curve[MAGNITUDES]
+            times = curve[TIMES]
+
+            slopes = feature_extract.curve_slopes(times, magnitudes)
+
+            return feature_extract.above_below_1std_slopes(slopes)
+
+        try_expected(self, TEST_CURVES, expected_results, test)
